65ffdd970745ebb136210bd7db469f4520b69e94
Rodiat Hammed <rodiathammed48@gmail.com>
2025-04-29 20:54:29 +0100

    feat: add middleware for logging request duration in FastAPI app

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+import time
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    end_time = time.time()
+
+    duration = end_time - start_time
+    print(f"Request: {request.method} {request.url} | Duration: {duration:.4f} seconds")
+    return response
+
+
+@app.get('/')
+def home():
+    return {"message": "Welcome to UserMoney"}
