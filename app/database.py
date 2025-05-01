8028ef12731c55509b7ad4befe64dc13a6302b84
Rodiat Hammed <rodiathammed48@gmail.com>
2025-05-01 10:41:06 +0100

    refactor: switch to Motor async MongoDB client and update collection access

--- app/database.py
+++ app/database.py
@@ -1,9 +1,14 @@
 import os
-from pymongo import mongo_client
 from dotenv import load_dotenv
+import motor.motor_asyncio
 
 load_dotenv()
 
-client = mongo_client.MongoClient(os.getenv("DB_URL"))
 
-users_collection = client["user_money_v2"]["users"]
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+db = client.user_money_v2
+
+
+users_collection = db.get_collection("users")
+accounts_collection = db.get_collection("accounts")
+transactions_collection = db.get_collection("transactions")
