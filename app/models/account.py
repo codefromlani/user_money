29aeb4ac5e9eb7c31efbfc81ad550e18ecf8307c
Rodiat Hammed <rodiathammed48@gmail.com>
2025-05-04 12:47:27 +0100

    feat(account): extend Account model with type, status, currency, and validation

--- app/models/account.py
+++ app/models/account.py
@@ -1,13 +1,18 @@
 from datetime import datetime
+from typing import Literal
 from bson import ObjectId
 from pydantic import BaseModel, Field, ConfigDict
 from decimal import Decimal
 
 
 class Account(BaseModel):
-    id: ObjectId = Field(default_factory=ObjectId, alias="_id")
-    user_id: ObjectId
+    id: str = None
+    user_id: str
+    account_type: Literal["savings", "current"] = "savings"
+    account_number: str = Field(..., min_length=10, max_length=10)
     balance: Decimal = Field(default=Decimal("0.00"))
+    is_active: bool = True
+    currency: str = "NGN"
     created_at: datetime = Field(default_factory=datetime.utcnow)
     updated_at: datetime = Field(default_factory=datetime.utcnow)
     
