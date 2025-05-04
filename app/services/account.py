6f5126f556a3d8c022f8fe1284dcf8e470b2b68a
Rodiat Hammed <rodiathammed48@gmail.com>
2025-05-04 21:02:59 +0100

    feat(account): add user account creation, retrieval, and balance fetching functionalities

---
+++ app/services/account.py
@@ -0,0 +1,78 @@
+from fastapi import HTTPException, status, Depends
+from typing import Literal
+from datetime import datetime
+from bson import ObjectId
+import random
+import string
+
+from ..database import accounts_collection, users_collection
+from ..models.user import User
+from ..services.auth import get_current_user
+from ..models.account import Account
+
+async def generate_account_number() -> str:
+    while True:
+        account_number = ''.join(random.choices(string.digits, k=10))
+
+        existing = await accounts_collection.find_one({"account_number": account_number})
+        if not existing:
+            return account_number
+        
+async def create_account_for_user(
+        bvn: str, 
+        account_type: Literal["savings", "current"] = "savings",
+        current_user: User = Depends(get_current_user),
+    ) -> dict:
+    if not current_user.bvn:
+        await users_collection.update_one(
+            {"_id": ObjectId(current_user.id)},
+            {
+                "$set": {"bvn": bvn},
+                "$currentDate": {"updated_at": True}
+            }
+        )
+    
+    elif current_user.bvn != bvn:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="The BVN provided does not match your registered BVN."
+        )
+    
+    existing_account = await accounts_collection.find_one({
+        "user_id": str(current_user.id),
+        "account_type": account_type
+    })
+    
+    if existing_account:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"You already have a {account_type} account."
+        )
+    
+    new_account = {
+        "user_id": str(current_user.id),
+        "account_type": account_type,
+        "account_number": await generate_account_number(),
+        "balance": 0.0,
+        "is_active": True,
+        "currency": "NGN",
+        "created_at": datetime.utcnow(),
+        "updated_at": datetime.utcnow()
+    }
+
+    result = await accounts_collection.insert_one(new_account)
+    new_account["_id"] = str(result.inserted_id)
+    return new_account
+
+async def get_user_account(user_id: str) -> Account:
+    account_data = await accounts_collection.find_one({"user_id": user_id})
+    if not account_data:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Account not found"
+        )
+    return Account(**account_data)
+
+async def get_user_balance(user_id: str) -> float:
+    account = await get_user_account(user_id)
+    return account.balance
